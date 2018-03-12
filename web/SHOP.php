035e78384b95dd46b1d17a6ec84ef3827869d099
olga-hry <30637796+olga-hry@users.noreply.github.com>
2018-03-12 20:05:44 +0300

    o

---
+++ web/SHOP.php
@@ -0,0 +1,570 @@
+﻿<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset=UTF-8>
+    <title>BOOT</title>
+<link rel="stylesheet" href="cs.css">
+<meta name="viewport" content="width=device-width, initial-scale=1"> 
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<link href="https://fonts.googleapis.com/css?family=Great+Vibes|Nunito" rel="stylesheet">
+<link rel="stylesheet" href="css/fontawesome-all.min.css">
+<link rel="stylesheet" href="css/navbar.css">
+<link rel="stylesheet" href="css/media.css">
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="icon" href="img/paper-bag.png">
+
+
+
+  </head>
+  <body>
+    
+ 
+
+ <div class="container"> 
+	<div class="container-fluid header" >
+			
+					<div class="nav coll col-lg-9 col-lg-offset-2 col-md-9 col-md-offset-2 col-sm-10 col-sm-offset-1   ">
+							
+
+          
+
+<div class="menu collapse navbar-collapse"><ul collapse navbar-collapse> 
+<li><a href="#NewArrival">New Arrival</a></li> 
+<li><a href="#Men">Men</a></li> 
+<li><a href="#Women">Women</a></li> 
+<li><a href="#Gift">Gift</a></li> 
+<li><a href="#Sale">Sale</a></li> 
+
+</ul></div> 
+
+
+
+</div>
+
+
+
+
+
+
+					
+
+<div class="nav col-lg-12  col-md-12  col-sm-12 ">
+
+					<div class="discount"><span>Discount on Clothing</span></div>
+					<div class="off">GET 50% OFF</div>
+					<div><button type='button'>BUY NOW</button></div>
+					</div>
+
+
+
+<div>
+<button  id="coll" type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"><i class="fas fa-align-justify"></i>
+              
+            </button></div>
+			
+	</div>
+</div>
+
+
+
+
+
+       
+   
+   
+
+
+
+
+
+
+
+<div class="container"> 
+	<div class="container-fluid welcom conter">
+			
+
+					<div class="block">
+					<h1>WELCOM TO OUR SHOP</h1>
+					<svg width=100% height=10><line x1=46% y1=5 x2=54% y2=5 stroke=grey stroke-width=3 stroke-opacity=0.5 /> </svg>
+					</div>
+
+				<div class="text col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1 col-sm-12 "> <p>Ut enimses ad minim veniam, quis nostrud exercitation ullamco laboris nisi utal quipes exe erra commodo consequat. Duis aute irure dolor in cillum eu fugiat nulla pariatur.</p> 
+				</div>
+
+							
+	</div>
+</div>
+
+
+
+<div class="container"> 
+
+
+			<div class="container-fluid free conter">
+					
+
+
+
+						<div class="col-lg-6 col-md-6  col-sm-6"> 
+						<div class="cont a">
+									<div class="texto">
+							<div class="symbol"><p><i class="fas fa-truck"></i></p></div>	
+							<h2>FREE SHIPPING</h2><p><span>ON ORDER OF &#36;50</span> </p>
+							 </div></div></div>
+						<div class="col-lg-6 col-md-6 col-sm-6">
+							<div class="cont b">
+									<div class="texto">
+
+								<div class="symbol"><p><i class="fas fa-sync-alt"></i></p></div>
+								<h2>EASY RETURN</h2><p><span>AT YOU DOORSTEP</span></p></div> </div> </div>
+					
+					
+
+			</div>
+
+</div>
+
+
+
+
+
+
+<div class="container"> 
+
+		<div class="container-fluid month conter">
+
+
+					<div class="row">
+									<div class="block">
+
+
+										<h1>BEST OFFER OFF MONTH</h1>
+										<svg width=100% height=10><line x1=46% y1=5 x2=54% y2=5 stroke=grey stroke-width=3 stroke-opacity=0.5 /> </svg>
+					
+									</div>
+					</div>
+							
+							
+					<div class="row ph">
+									<div class="col-lg-6 col-md-6  col-sm-6 col-xs-10"> 
+											<div class="cont a photo"> 
+												<img src="img/man.jpg" class="img-responsive center-block" alt="man"> 			
+														<h2>BOY DRESSING DESIGN</h2><p><span>&#36;299.99</span></p>
+												<div><button type='submit'>ADD TO CARD</button></div>
+												<div class="sign" id="best">BEST SALE</div> 
+												
+											</div> 
+									</div> 
+
+
+									<div class="col-lg-6 col-md-6 col-sm-6 hidden-xs"> 
+											
+												<div class="cont b photo"> 
+											
+												<img src="img/woman.jpg" class="img-responsive center-block" alt="woman">
+														<h2>GREY SHIRT DESIGN</h2><p><span>&#36;195.99</span></p>
+												<div><button type='submit'>ADD TO CARD</button></div>
+												<div class="sign" id="trend">TRENDING</div>
+												
+											</div> 
+																	
+									</div>	
+
+					</div>
+					<div class="row see visible-xs col-xs-12">
+
+									<div><p><a>VIEW MORE</a></p></div>
+
+
+
+					</div>						 					
+
+		</div>
+
+</div>
+
+
+
+
+<div class="container"> 
+			<div class="container-fluid port conter">
+
+							
+					<div class="row">
+
+										<div class="block">
+
+
+											<h1>PORTFOLIO &amp; OFFERS</h1>
+											<svg width=100% height=10><line x1=46% y1=5 x2=54% y2=5 stroke=grey stroke-width=3 stroke-opacity=0.5 /> </svg>
+										</div>
+					</div>
+
+
+					<div class="row ph">
+										<div class="col-lg-6 col-md-6  col-sm-6 col-xs-6  "> 
+												<div class="left"> 
+													<img src="img/girl.jpg" class="img-responsive center-block" alt="girl"> 
+													<div class="sale l"><p>BEST SALE<br><span>60% OFF</span></p></div>
+												</div>
+
+										</div> 
+
+
+										<div class="col-lg-6 col-md-6  col-sm-6 hidden-xs"> 
+											
+												<div class="right"> 
+
+													<img src="img/bag.jpg" class="img-responsive center-block" alt="bag"> 
+													<img src="img/feet.jpg" class="img-responsive center-block" alt="feet"> 
+												</div>
+
+										</div>
+					</div>
+
+
+
+					<div class="row see visible-xs col-xs-12">
+
+									<div><p><a>VIEW MORE</a></p></div>
+
+
+
+					</div>
+
+
+
+
+			</div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<div class="container"> 
+
+
+			<div class="container-fluid seasonal conter">
+
+
+					
+
+					<div class="row">
+								<div class="reclama col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1 col-sm-10 col-sm-offset-1"><p>Free standard shipping on orders over &#36;50</p><div><button type='submit'>VIEW OFFERS</button></div></div>
+					</div>
+
+
+					<div class="row">
+							<div class="block">
+								<h1>SEASONAL COLLECTIONS</h1>
+									<svg width=100% height=10><line x1=46% y1=5 x2=54% y2=5 stroke=grey stroke-width=3 stroke-opacity=0.5 /> </svg>
+							</div>
+					</div>
+							
+					
+					<div class="row ph">
+										<div class="col-lg-6 col-md-6  col-sm-6 hidden-xs"> 
+											
+												<div class="left"> 
+													<img src="img/head.jpg" class="img-responsive center-block" alt="head"> 
+													<img src="img/tie.jpg" class="img-responsive center-block" alt="tie"> 
+													<img src="img/hipster.jpg" class="img-responsive center-block" alt="hipster"> 
+												</div>
+										</div>
+
+
+										<div class="col-lg-6 col-md-6  col-sm-6 col-xs-6 "> 
+												<div class="right"> 
+													<img src="img/boy.jpg" class="img-responsive center-block" alt="boy"> 
+													<div class="sale r"><p>BEST SALE<br><span>60% OFF</span></p></div>
+													
+												</div>
+
+										</div>
+
+					</div>
+
+							
+					<div class="row">
+						<div class="block">
+							<div><button type='submit'>VIEW MORE</button></div> </div>
+					</div>
+
+
+
+					<div class="row see visible-xs col-xs-12">
+
+									<div><p><a>VIEW MORE</a></p></div>
+
+
+
+					</div>	
+
+			</div>
+</div>
+
+
+
+
+
+
+
+
+<div class="container"> 
+
+
+			<div class="container-fluid awesome conter">
+
+
+							<div class="block">
+								<h1>AWESOME PRICING</h1>
+									<svg width=100% height=10><line x1=46% y1=5 x2=54% y2=5 stroke=grey stroke-width=3 stroke-opacity=0.5 /> </svg>
+							</div>
+
+
+				<div class="row ph">
+										<div class="col-lg-6 col-md-6  col-sm-6"> 
+												<div class="cont a"> 
+														<div class="zagolovok"><p>70% OFF<br>MEN'S CLOTHING</p></div>
+														<div class="text spis"><p>Men's Shope</p> <p>Men's Jeans</p> <p>Men's Shoes</p> <p>Men's Accessories</p></div>
+														
+														<div class="sign" id="days3">3 DAYS LEFT</div> 
+														<div><button type='submit'>VIEW PLAN</button></div>
+												</div>
+										</div>
+
+										<div class="col-lg-6 col-md-6  col-sm-6"> 
+												<div class="cont b"> 
+														<div class="zagolovok"><p>78% OFF<br>WOMEN'S CLOTHING</p></div>
+														<div class="text spis"><p>Women's Shope</p>  <p>Women's Jeans</p> <p>Women's Shoes</p> <p>Women's Accessories</p></div>
+
+														<div class="sign" id="days8">8 DAYS LEFT</div> 	
+														<div><button type='submit'>VIEW PLAN</button></div>
+												</div>
+										</div>
+					</div>		
+				
+
+			</div>
+</div>
+
+
+
+<div class="container"> 
+
+
+			<div class="container-fluid film conter">
+
+
+						<div class="col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1 col-sm-10 col-sm-offset-1">
+							<div class="filma">
+								<iframe width="890" height="470" src="https://www.youtube.com/embed/ZT_FEUXJ9KY"   allowfullscreen></iframe>
+							</div>
+						</div>
+
+
+						<div class="reclama filma col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1 col-sm-10 col-sm-offset-1">
+							<p>580 &times; 80px</p>
+							<div>Advertisement</div>
+						</div>
+			</div>
+</div>
+
+
+
+
+
+
+
+
+
+<div class="container"> 
+
+
+			<div class="container-fluid blog conter">
+
+
+				
+
+							<div class="block">
+
+
+								<h1>BLOG LATEST NEWS</h1>
+									<svg width=100% height=10><line x1=46% y1=5 x2=54% y2=5 stroke=grey stroke-width=3 stroke-opacity=0.5 /> </svg>
+					
+							</div>
+
+
+							<div class="cont col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1 col-sm-10 col-sm-offset-1 col-xs-12"> 
+
+								<div class="l col-lg-6 col-md-6  col-sm-6 hidden-xs">
+
+
+									<img src="img/winter.jpg" class="img-responsive center-block" alt="winter"> 
+									<div><button type='submit'>DETAIL</button></div>
+									 
+
+								</div>
+
+								<div class="col-lg-6 col-md-6  col-sm-6">
+									
+								<div class="l"><h3>01</h3></div>
+									<div class="text news"><p><span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla...</span></p></div>
+
+								</div>
+
+
+<div class="row see visible-xs col-xs-12">
+
+									<div><button type='submit'>DETAIL</button></div>
+
+
+
+					</div>
+
+
+							</div>
+
+							<div class="cont col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1 col-sm-10 col-sm-offset-1 col-xs-12"> 
+							
+
+									<div class="col-lg-6 col-md-6  col-sm-6">
+									<div class="r"><h3>02</h3></div>
+									<div class="text news"><p><span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla...</span></p>	</div>
+
+<div class="row see visible-xs col-xs-12">
+
+									<div><button type='submit'>DETAIL</button></div>
+
+
+
+					</div>
+									
+
+								</div>
+
+								<div class="col-lg-6 col-md-6  col-sm-6 hidden-xs">
+								<img src="img/glasses.jpg" class="img-responsive center-block" alt="glasses">
+							
+								<div><button type='submit'>DETAIL</button></div>	
+								
+								</div>
+
+							</div>		
+			</div>
+</div>
+
+
+
+
+<div class="container"> 
+
+
+			<div class="container-fluid cust conter">
+
+
+					
+
+						<div class="cont col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1 col-sm-10 col-sm-offset-1 col-xs-12"> 
+							<div class="custom">
+													<div class="symbol cust"> <p> <i class="fas fa-quote-right"></i></p></div>
+													<div class="text"><h3>OUR HAPPY CUSTOMERS</h3></div>
+													<div class="tom">TOM <span>D'CRUZ</span></div>
+
+								<div ><div id="cent"><p><span >&ldquo;Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa.&rdquo;</span></p></div></div>
+							</div>
+						</div>
+			</div>
+</div>
+
+
+
+
+<div class="footer"> 
+
+
+			<div class="container-fluid foot">
+
+
+
+
+
+	<div class="row ">		
+
+<div class=" col-lg-3 col-lg-offset-3 col-md-3 col-md-offset-2 col-sm-3 col-sm-offset-2  "> 
+<div class=" t fi" id="f">
+	<h4>CONTACT US</h4>
+<p> <i class="fas fa-compass"></i>1600 Elizabeth St, Melbourne </p>
+<p><i class="fas fa-envelope"></i>contact@domain.com </p>
+<p><i class="fas fa-phone"></i>(+99) 22-44-22-222 </p>
+<p><i class="fas fa-phone"></i>(+98) 88-11-22-888 </p></div>
+
+							</div>
+
+
+<div class=" col-lg-2   col-md-3  col-sm-3 "> 
+<div class="t sm tall">
+<h4>COMPANY</h4>
+<p><i class="fas fa-angle-right"></i>Support</p>
+<p><i class="fas fa-angle-right"></i>Help Center </p>
+<p><i class="fas fa-angle-right"></i>Getting Staeted </p>
+<p><i class="fas fa-angle-right"></i>About </p>
+<p><i class="fas fa-angle-right"></i>Other </p>
+ 
+</div>
+</div>
+
+
+
+	<div class=" col-lg-2  col-md-2   col-sm-3  "> 
+
+<div class="t tall">
+<h4>PRODUCT</h4>
+<p> <i class="fas fa-angle-right"></i>Men's</p>
+<p><i class="fas fa-angle-right"></i>Women's </p>
+<p><i class="fas fa-angle-right"></i>Kids Clothing </p>
+<p><i class="fas fa-angle-right"></i>Accessories</p>
+<p><i class="fas fa-angle-right"></i>Other more</p></div>
+
+	</div>
+
+</div>
+
+<div class="row ph col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1">
+	<form  role="search">
+            <div class="form-group">
+              <input id="sea" type="text" class="form-control" placeholder="">
+            </div>
+              <button id="search" type="button"><i class="fas fa-search"></i> Search</button>                    
+          </form>
+        </div>
+
+
+
+
+</div></div>
+
+<div class="scrollup">
+      <i class="fas fa-angle-double-up"></i>
+  </div>
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="http://www.bootstrap-3.ru/dist/js/bootstrap.min.js"></script>
+<script src="javascr.js"></script>
+  </body>
+</html>
